ea2b6bbfddc80dcf3aabe2a8a3e00e493b0e3d74
Ankush Goyal <54682973+ankushgoyaljmv@users.noreply.github.com>
2020-09-15 11:03:30 +0530

    added post route

--- Facts/CRUD/api.js
+++ Facts/CRUD/api.js
@@ -6,14 +6,33 @@
 
 const express = require("express");
 const app = express();
+const userDB = require("./user.json");
+const fs = require("fs");
+const { memory } = require("console");
 // REST API 
 // HTTP methods =>
 // create  => POST
 // read => GET
-app.get("/api/users", function (req, res) {
-    console.log("Recieved req");
-    res.status(200).json({
-        status: "success recieved get request from client",
+// app.get("/api/users", function (req, res) {
+//     console.log("Recieved req");
+//     res.status(200).json({
+//         status: "success recieved get request from client",
+//     })
+// })
+
+app.use(express.json());
+//create => post
+app.post("api/users", function (req, res) {
+    let user = req.body;
+    // db save 
+    console.log(user);
+    // if a new entry is created on server
+    // memory => ram 
+    userDB.push(user);
+    fs.writeFileSync(path.join(__dirname, "user.json"), JSON.stringify(userDB));
+    res.status(201).json({
+        success: "successfull",
+        user: user
     })
 })
 // update  => PATCH
